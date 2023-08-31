051797933d13272b95d2791858e88819ecb8f97c
Noah Gerber <noahgerber100@gmail.com>
2023-08-31 19:27:14 +0200

    prettier astro plugin

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
