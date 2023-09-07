c848b1750aada0a810ba050cdc2731560a4c347c
Noah Gerber <noahgerber100@gmail.com>
2023-09-07 02:58:56 +0200

    add production domain

--- src/auth0.ts
+++ src/auth0.ts
@@ -1,8 +1,6 @@
 import { auth0Token } from './stores/tokenStore';
 
-export const ROOT = import.meta.env.DEV
-  ? 'http://localhost:4321'
-  : 'https://feature-login.whu-homepage-astro.pages.dev';
+export const ROOT = import.meta.env.DEV ? 'http://localhost:4321' : 'http://whu.app';
 
 const url: string = 'https://whuapp.eu.auth0.com';
 const clientId: string = 'LVuz7aq6FbmSk7Gc1MWiOVkHvHs7SDTW';
