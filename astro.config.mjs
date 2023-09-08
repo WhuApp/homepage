69fc661837ab522bb96023240c42e3cac9ffbfb9
Noah Gerber <noahgerber100@gmail.com>
2023-09-08 03:52:11 +0200

    ✨ Implement working delete user button (#8)
    * Feature/cloudflare preview (#5)
    
    * fix errot type check
    
    * add different preview and production environment roots
    
    * add AuthError typeGuard
    
    * Feature/cloudflare preview (#6)
    
    * fix errot type check
    
    * add different preview and production environment roots
    
    * add AuthError typeGuard
    
    * make root env public
    
    * add delete user badge
    
    * fix delete user endpoint
    
    * add api to allowed origin
    
    * add * to allowed origin
    
    * remove allowed origin
    
    * fix statuscodes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     mode: 'directory',
-    functionPerRoute: true
+    functionPerRoute: true,
   }),
   image: {
-    service: passthroughImageService()
+    service: passthroughImageService(),
   },
   integrations: [react()],
 });
---
+++ src/components/delete-user-badge.tsx
@@ -0,0 +1,46 @@
+import { useStore } from '@nanostores/react';
+import { auth0Token } from '../stores/tokenStore';
+import { logoutFlow, type AuthToken } from '../auth0';
+import { useState } from 'react';
+
+const BASE_URL = 'https://api.whu.app/users/v1/';
+
+function DeleteUserBadge() {
+  const [message, setMessage] = useState<string>('');
+  const authToken: AuthToken = JSON.parse(useStore(auth0Token));
+  if (useStore(auth0Token)) {
+    return (
+      <button
+        onClick={async () => {
+          const message = await deleteOwnUser(authToken);
+          setMessage(message);
+        }}
+      >
+        Delete User
+      </button>
+    );
+  } else {
+    return (
+      <>
+        <p>Not logged in</p>
+        <p>{message}</p>
+      </>
+    );
+  }
+}
+
+const deleteOwnUser = async (authToken: AuthToken): Promise<string> => {
+  const response = await fetch(BASE_URL + 'me', {
+    method: 'DELETE',
+    headers: { Authorization: 'Bearer ' + authToken.id_token },
+  });
+
+  if (response.status === 204) {
+    logoutFlow();
+    return 'Successfully deleted';
+  }
+
+  return await response.text();
+};
+
+export default DeleteUserBadge;
