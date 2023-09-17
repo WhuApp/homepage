100d5f28a48e454ee44825e400b8c06444c77f4f
Nils Osswald <imrebux@gmail.com>
2023-09-17 15:50:48 +0200

    :heavy_plus_sign: Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     mode: 'directory',
-    functionPerRoute: true,
+    functionPerRoute: true
   }),
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
-  integrations: [react()],
+  integrations: [react(), tailwind()]
 });
